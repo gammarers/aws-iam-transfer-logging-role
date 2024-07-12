e33082735a17a466f21bb2fbfdc73de3895addfb
yicr <yicr@users.noreply.github.com>
2024-07-12 18:40:06 +0900

    chore: project created with projen

---
+++ .projenrc.ts
@@ -0,0 +1,29 @@
+import { awscdk, javascript } from 'projen';
+const project = new awscdk.AwsCdkConstructLibrary({
+  author: 'yicr',
+  authorAddress: 'yicr@users.noreply.github.com',
+  authorOrganization: true,
+  cdkVersion: '2.80.0',
+  defaultReleaseBranch: 'main',
+  typescriptVersion: '5.4.x',
+  jsiiVersion: '5.4.x',
+  name: '@gammarers/aws-iam-transfer-logging-role',
+  projenrcTs: true,
+  repositoryUrl: 'https://github.com/gammarers/aws-iam-transfer-logging-role.git',
+  releaseToNpm: false, // temporary
+  npmAccess: javascript.NpmAccess.PUBLIC,
+  depsUpgrade: true,
+  depsUpgradeOptions: {
+    workflowOptions: {
+      labels: ['auto-approve', 'auto-merge'],
+      schedule: javascript.UpgradeDependenciesSchedule.expressions(['5 16 * * *']), // everyday
+    },
+  },
+  minNodeVersion: '18.0.0',
+  workflowNodeVersion: '22.x',
+  autoApproveOptions: {
+    secret: 'GITHUB_TOKEN',
+    allowedUsernames: ['yicr'],
+  },
+});
+project.synth();
